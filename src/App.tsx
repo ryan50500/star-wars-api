315a3af30324fb4cf062432b012b641bf5f7d25f
ryan woods <federer5050@gmail.com>
2023-09-19 17:33:48 +0100

    start up files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchBar from './components/SearchBar';
+import ResultsPage from './components/ResultsPage';
+
+function App() {
+  return (
+    <>
+      <SearchBar />
+      <ResultsPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ResultsPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResultsPage = () => {
+    return (
+        <div>
+            results here
+        </div>
+    )
+}
+
+export default ResultsPage
